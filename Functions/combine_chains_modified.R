ff68147b6ea1c6c6ddff3d22540144ac6113d7fa
FrancescaMancini <framan@ceh.ac.uk>
2020-01-24 14:51:48 +0000

    Tidy up the folder

--- Functions/combine_chains_modified.R
+++ Functions/combine_chains_modified.R
@@ -1,3 +1,8 @@
+# This is a modified version of the combine_chains function
+# for model runs where the three chains are in the same model output file
+# also contains an extra parameter to specify the model type
+# (integrated or not)
+
 # This function will attempt to identify the final daisy
 # for each species and then combine the chains, saving
 # a summary table. This assumes all species have the SAME
@@ -14,11 +19,14 @@
 # equal to the end value of a daisy (if not it is rounded up to the nearest)
 # overwrite - When false species will be skipped where results already exist
 # species_list - a character vetor giveing hte species to summarise. If NULL (default), all species are run
+# model_type - "integrated" or "not_integrated"
 library(readr)
+library(abind)
 combine_chains <- function(data_dir, output_dir = data_dir,
                            target = NULL, end_point = 'max',
                            verbose = TRUE, overwrite = FALSE,
-                           species_list = NULL){
+                           species_list = NULL,
+                           model_type = "not_integrated"){
   
   # Combine chains output on jasmin
   library(coda)
@@ -31,7 +39,6 @@ combine_chains <- function(data_dir, output_dir = data_dir,
   split <- strsplit(basename(all_files), split = 'it')
   file_info <- data.frame(file = all_files,
                           final_iteration = sapply(split , function(x) parse_number(x[length(x)])),
-                          chain = as.numeric(gsub('.rdata', '', sapply(split , function(x) tail(x,1)))),
                           stringsAsFactors = FALSE)
   # If we are using an alternative end point then find
   # it and forget all the other files
@@ -77,49 +84,63 @@ combine_chains <- function(data_dir, output_dir = data_dir,
     species_info <- file_info[file_info$file %in% species_files,]
     
     # We will always need to load in the last chains
-    chain_files <- species_info$file[species_info$final_iteration == max(species_info$final_iteration)]
+    last_it_file <- species_info$file[species_info$final_iteration == max(species_info$final_iteration)]
     
     if(verbose) cat(paste0(sp,'\n'))
     
-    sims.list <- lapply(unique(species_info$chain), FUN = function(chain){
-      
-      if(verbose) cat(paste0('Chain-', chain, ': '))
-      
-      chainfiles <- species_info[species_info$chain == chain, ]
-      
-      # Load the last chain
-      last_chain <- chainfiles$file[chainfiles$final_iteration == max(chainfiles$final_iteration)]
-      load(last_chain)
-      sims.matrix <- out_integrated_model$BUGSoutput$sims.matrix
+    load(last_it_file)
+    if(model_type == "not_integrated"){
+      out <- out_HRS_model
+      rm('out_HRS_model')
+    } else {
+      out <- out_integrated_model
       rm('out_integrated_model')
-      if(verbose) cat(paste0('it-', nrow(sims.matrix), ' '))
-      
-      if(!is.null(target)){
-        if(nrow(sims.matrix) >= target){
-          sims.matrix <- tail(sims.matrix, target)
-          if(verbose) cat(paste0('trimmed to it-', nrow(sims.matrix), ' '))
-        } else {
-          daisies <- sort(decreasing = TRUE, unique(species_info$final_iteration))
-          d <- 2
-          while(nrow(sims.matrix) < target){
-            if(d > length(daisies)) stop('Your target number of iterations is larger than the number available. NOTE: target refers to the number of iterations AFTER thinning.')
-            load(chainfiles$file[chainfiles$final_iteration == daisies[d]])
-            d <- d + 1
-            sims.matrix_annex <- out$BUGSoutput$sims.matrix
-            sims.matrix <- rbind(sims.matrix_annex, sims.matrix)
-            rm('sims.matrix_annex')
-            if(verbose) cat(paste0('it-', nrow(sims.matrix), ' '))
-            if(nrow(sims.matrix) > target){
-              sims.matrix <- tail(sims.matrix, target)
-              if(verbose) cat(paste0('trimmed to it-', nrow(sims.matrix), ' '))
-            }
+    }
+    sims.list <- out$BUGSoutput$sims.array
+    rm('out')
+    if(verbose) cat(paste0('it-', nrow(sims.list), ' '))
+    if(!is.null(target)){
+      if(nrow(sims.list) >= target){
+        sims.list <- sims.list[1:target,,]
+        if(verbose) cat(paste0('trimmed to it-', nrow(sims.list), ' '))
+      } else {
+        daisies <- sort(decreasing = TRUE, unique(species_info$final_iteration))
+        d <- 2
+        while(nrow(sims.list) < target){
+          if(d > length(daisies)) stop('Your target number of iterations is larger than the number available. NOTE: target refers to the number of iterations AFTER thinning.')
+          load(species_info$file[species_info$final_iteration == daisies[d]])
+          d <- d + 1
+          if(model_type == "not_integrated"){
+            out <- out_HRS_model
+            rm('out_HRS_model')
+          } else {
+            out <- out_integrated_model
+            rm('out_integrated_model')
+          }
+          sims.list_annex <- out$BUGSoutput$sims.array
+          sims.list <- abind(sims.list, sims.list_annex, along = 1)
+          rm('sims.list_annex')
+          if(verbose) cat(paste0('it-', nrow(sims.list), ' '))
+          if(nrow(sims.list) > target){
+            sims.list <- sims.list[1:target,,]
+            if(verbose) cat(paste0('trimmed to it-', nrow(sims.list), ' '))
           }
         }
       }
-      if(verbose) cat('\n')
-      return(as.mcmc(sims.matrix))
-      
-    })
+    }
+    if(verbose) cat('\n')
+    sims.list <- list(chain_1 = matrix(sims.list[1:target,1, 1:dim(sims.list)[3]], 
+                                       nrow = target, ncol =dim(sims.list)[3], 
+                                       dimnames = list(NULL, attr(sims.list, "dimnames")[[3]])),
+                      chain_2 = matrix(sims.list[1:target,2, 1:dim(sims.list)[3]], 
+                                       nrow = target, ncol =dim(sims.list)[3], 
+                                       dimnames = list(NULL, attr(sims.list, "dimnames")[[3]])),
+                      chain_3 = matrix(sims.list[1:target,3, 1:dim(sims.list)[3]], 
+                                       nrow = target, ncol =dim(sims.list)[3], 
+                                       dimnames = list(NULL, attr(sims.list, "dimnames")[[3]])))
+    sims.list <- lapply(sims.list, as.mcmc)
+    
+####### it stops working at this point because the array object cannot be turned into an mcmc object ######
     
     # convert these to a mcmc.list
     comb.samples <- mcmc.list(sims.list)
@@ -234,63 +255,63 @@ combine_chains <- function(data_dir, output_dir = data_dir,
 
 # csvfiles is a vector of paths to all the files to use
 
-inspect_convergence <- function(csvfiles){
-  
-  require(scales)
-  
-  master.psi <- NULL
-  
-  for(file in csvfiles){
-    csv_data <- read.csv(file = file, stringsAsFactors = FALSE)
-    csv_data <- csv_data[grep('^psi.fs', csv_data$X), ]
-    temp <- data.frame(year = csv_data$X,
-                       species = paste(head(strsplit(basename(file), '_')[[1]], -2),
-                                       collapse = '_'),
-                       end_point = as.numeric(gsub('.csv$', '', gsub('^ep', '', strsplit(basename(file), '_')[[1]][4]))),
-                       rhat = csv_data$PSRF)
-    master.psi <- rbind(master.psi, temp)
-  }
-  
-  master.psi$year <- as.numeric(gsub('\\]$', '', gsub('^psi.fs\\[', '', master.psi$year)))
-  
-  
-  master.pdet <- NULL
-  
-  for(file in csvfiles){
-    csv_data <- read.csv(file = file, stringsAsFactors = FALSE)
-    csv_data <- csv_data[grep('^pdet.alpha', csv_data$X), ]
-    temp <- data.frame(year = csv_data$X,
-                       species = paste(head(strsplit(basename(file), '_')[[1]], -2),
-                                       collapse = '_'),
-                       end_point = as.numeric(gsub('.csv$', '', gsub('^ep', '', strsplit(basename(file), '_')[[1]][4]))),
-                       rhat = csv_data$PSRF)
-    master.pdet <- rbind(master.pdet, temp)
-  }
-  
-  master.pdet$year <- as.numeric(gsub('\\]$', '', gsub('^pdet.alpha\\[', '', master.pdet$year)))
-  
-  
-  par(mfrow = c(1,2))
-  plot(x = master.psi$end_point,
-       y = master.psi$rhat,
-       pch = 4,
-       col = alpha(as.numeric(as.factor(master.psi$species)), 0.5),
-       xlab = 'Iterations',
-       ylab = 'rHat',
-       main = 'psi.fs')
-  abline(h = 1.1, col = 'red')
-  plot(x = master.pdet$end_point,
-       y = master.pdet$rhat,
-       pch = 4,
-       col = alpha(as.numeric(as.factor(master.pdet$species)), 0.5),
-       xlab = 'Iterations',
-       ylab = 'rHat',
-       main = 'pdet.alpha')
-  abline(h = 1.1, col = 'red')
-  
-  return(list(master.psi = master.psi,
-              master.pdet = master.pdet))
-}
+# inspect_convergence <- function(csvfiles){
+#   
+#   require(scales)
+#   
+#   master.psi <- NULL
+#   
+#   for(file in csvfiles){
+#     csv_data <- read.csv(file = file, stringsAsFactors = FALSE)
+#     csv_data <- csv_data[grep('^psi.fs', csv_data$X), ]
+#     temp <- data.frame(year = csv_data$X,
+#                        species = paste(head(strsplit(basename(file), '_')[[1]], -2),
+#                                        collapse = '_'),
+#                        end_point = as.numeric(gsub('.csv$', '', gsub('^ep', '', strsplit(basename(file), '_')[[1]][4]))),
+#                        rhat = csv_data$PSRF)
+#     master.psi <- rbind(master.psi, temp)
+#   }
+#   
+#   master.psi$year <- as.numeric(gsub('\\]$', '', gsub('^psi.fs\\[', '', master.psi$year)))
+#   
+#   
+#   master.pdet <- NULL
+#   
+#   for(file in csvfiles){
+#     csv_data <- read.csv(file = file, stringsAsFactors = FALSE)
+#     csv_data <- csv_data[grep('^pdet.alpha', csv_data$X), ]
+#     temp <- data.frame(year = csv_data$X,
+#                        species = paste(head(strsplit(basename(file), '_')[[1]], -2),
+#                                        collapse = '_'),
+#                        end_point = as.numeric(gsub('.csv$', '', gsub('^ep', '', strsplit(basename(file), '_')[[1]][4]))),
+#                        rhat = csv_data$PSRF)
+#     master.pdet <- rbind(master.pdet, temp)
+#   }
+#   
+#   master.pdet$year <- as.numeric(gsub('\\]$', '', gsub('^pdet.alpha\\[', '', master.pdet$year)))
+#   
+#   
+#   par(mfrow = c(1,2))
+#   plot(x = master.psi$end_point,
+#        y = master.psi$rhat,
+#        pch = 4,
+#        col = alpha(as.numeric(as.factor(master.psi$species)), 0.5),
+#        xlab = 'Iterations',
+#        ylab = 'rHat',
+#        main = 'psi.fs')
+#   abline(h = 1.1, col = 'red')
+#   plot(x = master.pdet$end_point,
+#        y = master.pdet$rhat,
+#        pch = 4,
+#        col = alpha(as.numeric(as.factor(master.pdet$species)), 0.5),
+#        xlab = 'Iterations',
+#        ylab = 'rHat',
+#        main = 'pdet.alpha')
+#   abline(h = 1.1, col = 'red')
+#   
+#   return(list(master.psi = master.psi,
+#               master.pdet = master.pdet))
+# }
 
 brackOmit <- function(x){
   
--- restartHRSjobs.R
+++
@@ -1,7 +0,0 @@
-source("restart_failures.R")
-source("job_status.R")
-
-r_path <- restart_failures(dir = getwd(), originalJob = "bsub_lines.sh", remove_logs = TRUE, datafiles_path = "Outputs/HRS/", 
-                           consolefiles_path = "Outputs/Console", logfiles_path = "Outputs/Log_files", 
-                           roster_file = "Data/parameters.csv", job_file = "multi_array.job")
-                           
